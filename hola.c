231dc813c2570b2f4286f15f4e6efc4239206f86
Ilyas Ouacham Rhailane <ilyas.ouacham@estudiantat.upc.edu>
2024-10-25 13:43:59 +0200

    Update hola.c

--- hola.c
+++ hola.c
@@ -1,7 +1,19 @@
+#include <unistd.h>
 #include <stdio.h>
+#include <time.h>
 
-int main(void)
-{
-  printf("\aHello world");
-  return 0;
+#define S_TO_MS(x) (x*1000000)
+
+int main() {
+    int inicial = 2;
+    int velocidad = 0.1;
+    int interval = S_TO_MS(inicial); // 0.5 seconds
+    
+    for (int i = 0; i < 10; i++) {
+        printf("\a%d ", i);
+        usleep(interval);
+        
+        interval = S_TO_MS((float) inicial - (float) velocidad*i);
+    }
+    return 0;
 }
